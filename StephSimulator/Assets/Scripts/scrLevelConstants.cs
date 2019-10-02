5e965251541313db0693404b634dae11b44b1225
weesteekygames <weesteekygames@outlook.com>
2019-10-02 18:51:58 +0100

    New sprites and script

---
+++ StephSimulator/Assets/Scripts/scrLevelConstants.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace levelConstants {
+    public class scrLevelConstants : MonoBehaviour
+    {
+        public static float V_VERTICAL_MOVEMENT = 8;
+    }
+}
